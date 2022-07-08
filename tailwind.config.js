d6463f8c8f394d14f13a3037be61aae985f80020
Vlad Gerasimovich <hotepp@pm.me>
2022-07-08 21:25:08 +0200

    feat: add container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        lg: "2rem",
+      },
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp")],
