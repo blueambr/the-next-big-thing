6f768d270bd44b0f0e6e2f2660598756fbb7ee35
Vlad Gerasimovich <hotepp@pm.me>
2022-07-09 09:55:56 +0200

    feat: update project structure, update eslint, lint-staged configs

---
+++ components/00-layout/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const Layout = ({ children, data }) => {
+  const { title } = data;
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="favicon/manifest.json" />
+        <link rel="shortcut icon" href="favicon/favicon.ico" />
+        <title>{title}</title>
+      </Head>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
